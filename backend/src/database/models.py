077e1ea7ea1babd30a2d882c61265d84f7afdbf5
JGW0204 <ckwone12@naver.com>
2024-11-17 16:18:49 +0900

    update

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,5 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, MetaData
 from database import Base
+
+user_game_result = Table(
+    'user_game_result',
+    metadata=MetaData(),
+    id = Column(Integer, primary_key=True, index=True),
+    game_name = Column(String, nullable=False),
+    how_many_play = Column(Integer, nullable=False),
+    how_many_win = Column(Integer, nullable=False)
+)
 
 class Door(Base):
     __tablename__= "Door"
@@ -15,4 +24,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False)
     pw = Column(String)
-    
+
