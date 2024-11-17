9977f00a76af8de6c759fcedd64580cf40c20f15
JGW0204 <ckwone12@naver.com>
2024-11-17 16:31:15 +0900

    update

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,17 +1,9 @@
 from sqlalchemy import Column, Integer, String, Table, MetaData
 from database import Base
 
-user_game_result = Table(
-    'user_game_result',
-    metadata=MetaData(),
-    id = Column(Integer, primary_key=True, index=True),
-    game_name = Column(String, nullable=False),
-    how_many_play = Column(Integer, nullable=False),
-    how_many_win = Column(Integer, nullable=False)
-)
 
 class Door(Base):
-    __tablename__= "Door"
+    __tablename__ = "Door"
 
     id = Column(Integer, primary_key=True, index=True)
     room_num = Column(Integer, unique=True, nullable=True)
@@ -19,9 +11,17 @@ class Door(Base):
 
 
 class User(Base):
-    __tablename__= "User"
+    __tablename__ = "User"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False)
     pw = Column(String)
 
+
+class Game_result(Base):
+    __tablename__ = "Game_result"
+
+    user_name = Column(String, unique=True, nullable=False)
+    game_name = Column(String, unique=True, nullable=False)
+    how_many_play= Column(Integer, nullable=False)
+    how_many_win = Column(Integer, nullable=False)
