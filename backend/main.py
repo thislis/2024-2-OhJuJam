3956f4bf3325813737bb73461f2fa3e4f2a7b3a4
JGW0204 <ckwone12@naver.com>
2024-11-18 21:06:08 +0900

    update

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from backend.src.api import sign_in
 from backend.src.api import sign_up
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(sign_up.router)
 app.include_router(sign_in.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
