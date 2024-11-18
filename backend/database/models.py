80c397d605cb248dd6e3558d66c61d75df4c3e75
JGW0204 <ckwone12@naver.com>
2024-11-18 21:11:20 +0900

    update

--- backend/database/models.py
+++ backend/database/models.py
@@ -26,5 +26,7 @@ class Game_result(Base):
     id = Column(Integer, primary_key=True, index=True)
     user = relationship("User")
     game_name = Column(String, unique=True, nullable=False)
-    how_many_play= Column(Integer, nullable=False)
+    where_to_play = Column(Integer, primary_key=True, index=True)
+    how_many_play = Column(Integer, nullable=False)
+    where_to_win = Column(Integer, primary_key=True, index=True)
     how_many_win = Column(Integer, nullable=False)
