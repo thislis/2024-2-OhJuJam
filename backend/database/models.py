aef814571b316b944c308a47b9c93428c879569b
JGW0204 <ckwone12@naver.com>
2024-11-20 01:36:41 +0900

    update

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from database.database import Base
 
 
@@ -9,23 +8,3 @@ class Door(Base):
     id = Column(Integer, primary_key=True, index=True)
     room_num = Column(Integer, unique=True, nullable=True)
     opened = Column(Integer, unique=True)
-
-
-class User(Base):
-    __tablename__ = "User"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
-    game_result = relationship("Game_result")
-
-
-class Game_result(Base):
-    __tablename__ = "Game_result"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user = relationship("User")
-    game_name = Column(String, unique=True, nullable=False)
-    where_to_play = Column(Integer, primary_key=True, index=True)
-    how_many_play = Column(Integer, nullable=False)
-    where_to_win = Column(Integer, primary_key=True, index=True)
-    how_many_win = Column(Integer, nullable=False)
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
 
-from src.api import sign_in, sign_up, open_door
+from src.api import open_door
 
 app = FastAPI()
-app.include_router(sign_up.router)
-app.include_router(sign_in.router)
 app.include_router(open_door.router)
 
 app.add_middleware(
@@ -18,4 +17,8 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
+    return RedirectResponse(url="/main", status_code=302)
+
+@app.get("/main")
+async def home():
     return {"message": "Main"}
