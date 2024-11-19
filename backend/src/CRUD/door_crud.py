4bac3ec9c4c8e479dcd60ba54c2d16fe3d498822
JGW0204 <ckwone12@naver.com>
2024-11-19 22:50:02 +0900

    update

--- backend/src/CRUD/door_crud.py
+++ backend/src/CRUD/door_crud.py
@@ -27,14 +27,7 @@ def create_door(db: Session, door_create: DoorCreate):
     return door_num
 
 
-def get_existing_door(db: Session, door_create: DoorCreate):
-    return db.query(Door).filter(
-        (Door.room_num == door_create.door_num)
-    ).first()
-
-
 def update_door(db: Session, door_num: int, door_open: int):
     value = door_open * db.query(Door).filter_by(door_num=door_num).opened
     db.query(Door).filter_by(door_num=door_num).update({"opened": value})
     db.commit()
-    
